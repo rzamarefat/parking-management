2f6fbda41bbce606fe14b37a1911abce8dbcb2f9
rzamarefat <rzamarefat@gmail.com>
2024-08-13 17:32:06 +0330

    dbhandler all methods

--- producer/Configuration.py
+++ producer/Configuration.py
@@ -7,3 +7,10 @@ class Configuration:
     RABBIT_CREDENTIALS_PASSWORD = "guest"
     RABBIT_IP = "127.0.0.1"
     QUEUE_NAME = "parking-producer"
+
+    DB_NAME = "Parking" 
+    DB_USER = "postgres"
+    DB_PASSWORD = "postgres"
+    DB_HOST = "localhost"
+    DB_PORT = "5432"
+    DB_TABLE_NAME = "public.parking"
--- producer/DatabaseHandler.py
+++ producer/DatabaseHandler.py
@@ -1,46 +1,75 @@
 import psycopg2
 from psycopg2 import sql, Error
+from Configuration import Configuration as CONFIG
+from psycopg2.extras import Json
 
-# Define your connection parameters
-DB_PARAMS = {
-    'dbname': 'Parking',
-    'user': 'postgres',
-    'password': 'postgres',
-    'host': 'localhost',    # e.g., 'localhost'
-    'port': '5432'     # e.g., '5432'
-}
-
-try:
-    # Establish the connection
-    connection = psycopg2.connect(**DB_PARAMS)
-    cursor = connection.cursor()
-
-    # Print PostgreSQL Connection properties
-    print("PostgreSQL server information")
-    print(connection.get_dsn_parameters(), "\n")
-
-    # Execute a query
-    cursor.execute("SELECT version();")
-
-    # Fetch the result
-    db_version = cursor.fetchone()
-    print("You are connected to - ", db_version, "\n")
-
-    # Example of executing a query and fetching data
-    query = sql.SQL("SELECT * FROM public.parking;")
-    cursor.execute(query)
-    records = cursor.fetchall()
-    print(records)
-    for row in records:
-        print(row)
-
-except (Exception, Error) as error:
-    print("Error while connecting to PostgreSQL", error)
-
-finally:
-    # Closing the cursor and connection
-    if cursor:
+class DatabaseHandler:
+    def __init__(self):
+        self._db_params = {
+                'dbname': CONFIG.DB_NAME,
+                'user': CONFIG.DB_USER,
+                'password': CONFIG.DB_PASSWORD,
+                'host': CONFIG.DB_HOST,
+                'port': CONFIG.DB_PORT,    
+                }
+        
+    def get_last_not_analyzed_index(self, timestamp):
+        query = f"""SELECT * FROM {CONFIG.DB_TABLE_NAME} 
+                    WHERE is_analyzed=false
+                    AND timestamp='{timestamp}' 
+                    ORDER BY index DESC;
+                """
+        connection = psycopg2.connect(**self._db_params)
+        cursor = connection.cursor()
+        query = sql.SQL(query)
+        cursor.execute(query)
+        records = cursor.fetchall()
         cursor.close()
-    if connection:
         connection.close()
-    print("PostgreSQL connection is closed")
+
+        latest_index = [r[0] for r in records][0]
+        
+        return latest_index
+
+    def update_frame_stats(self, index, timestamp, stat):
+        connection = psycopg2.connect(**self._db_params)
+        cursor = connection.cursor()
+
+        query = sql.SQL(f"""
+            UPDATE {CONFIG.DB_TABLE_NAME}
+            SET stats = %s, is_analyzed=true
+            WHERE index = %s
+            AND timestamp = %s;
+        """)
+
+        cursor.execute(query, (Json(stat), index, timestamp))
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+        
+    
+    def push_frame_to_db(self, index, timestamp):
+        connection = psycopg2.connect(**self._db_params)
+        cursor = connection.cursor()
+
+        query = sql.SQL(f"""
+            INSERT INTO {CONFIG.DB_TABLE_NAME} (index, timestamp, is_analyzed)
+            VALUES (%s, %s, %s);
+        """)
+
+        cursor.execute(query, (index, timestamp, False))
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+
+
+
+
+if __name__ == "__main__":
+    db_handler = DatabaseHandler()
+    # res = db_handler.get_last_not_analyzed_index("2023-08-08")
+    
+    db_handler.update_frame_stats(index='0', timestamp="2023-08-08", stat={"dsadasda": {"aasas": "a"}})
+    # db_handler.push_frame_to_db("2", "2023-08-08")
